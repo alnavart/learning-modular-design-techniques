46516541fa0a6ed0736f6adc23e65e7912e7f6b3
alberto.navarrete@rumbo.com <Alberto Navarrete>
2016-10-13 16:02:44 +0200

    read from the scanner and shows in the display

--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.refEq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -17,21 +18,23 @@ public class ShopTest
 
   private String product1Barcode = "10100011";
   private String product2Barcode = "10100012";
+  private Integer product1Price = 9;
+  private Integer product2Price = 10;
 
   @Test
   public void getsProduct1Price()
   {
-    int price = Cashier.getPrice(product1Barcode);
+    Integer price = BarcodeToPriceConverter.getPrice(product1Barcode);
 
-    assertEquals(9, price);
+    assertEquals(product1Price, price);
   }
 
   @Test
   public void getsProduct2Price()
   {
-    int price = Cashier.getPrice(product2Barcode);
+    Integer price = BarcodeToPriceConverter.getPrice(product2Barcode);
 
-    assertEquals(10, price);
+    assertEquals(product2Price, price);
   }
 
   @Test
@@ -61,7 +64,19 @@ public class ShopTest
     verify(hardwareDisplay).show("hello");
   }
 
-  private static class Cashier
+  @Test
+  public void showsProduct1PriceUseCase()
+  {
+    when(hardwareCashierScanner.read()).thenReturn(product1Barcode);
+    CashierScanner cashierScanner = new CashierScanner(hardwareCashierScanner);
+    Cashier cashier = new Cashier(cashierScanner, hardwareDisplay);
+
+    cashier.scan();
+
+    verify(hardwareDisplay).show(product1Price.toString());
+  }
+
+  private static class BarcodeToPriceConverter
   {
     public static int getPrice(String barcode)
     {
@@ -108,6 +123,20 @@ public class ShopTest
     public void show(String text)
     {
       hardwareDisplay.show(text);
+    }
+  }
+
+  @AllArgsConstructor
+  private class Cashier
+  {
+    CashierScanner cashierScanner;
+    HardwareDisplay hardwareDisplay;
+
+    public void scan()
+    {
+      String barcode = hardwareCashierScanner.read();
+      Integer price = BarcodeToPriceConverter.getPrice(barcode);
+      hardwareDisplay.show(price.toString());
     }
   }
 }
