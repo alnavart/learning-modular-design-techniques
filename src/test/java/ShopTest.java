251908b96ebf2ead327fa0495069ee6bfebe064c
alberto.navarrete@rumbo.com <Alberto Navarrete>
2016-10-13 15:14:26 +0200

    two products shop

--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -4,20 +4,31 @@ import static org.junit.Assert.assertEquals;
 public class ShopTest
 {
   @Test
-  public void cashier()
+  public void oneProductPrice()
   {
-    String barcode = "10100011";
-
-    int price = Cashier.read(barcode);
+    int price = Cashier.read("10100011");
 
     assertEquals(9, price);
+  }
+
+  @Test
+  public void otherProductPrice()
+  {
+    int price = Cashier.read("10100012");
+
+    assertEquals(10, price);
   }
 
   private static class Cashier
   {
     public static int read(String barcode)
     {
-      return 9;
+      if(barcode.equals("10100011"))
+      {
+        return 9;
+      }else {
+        return 10;
+      }
     }
   }
 }
